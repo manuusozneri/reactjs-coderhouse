a74278debc545b96768eed60ec4ba8df805f0ca6
Manuel Usoz <manuelusozneri@gmail.com>
2022-02-02 17:49:18 -0300

    navbar part 1

---
+++ src/components/Navbar/MenuItem.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Productos',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Nosotros',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contactar',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Iniciar Sesion',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+]
