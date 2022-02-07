ad3c390f164d7fe8564f0837f0e13c3fd562f28e
Manuel Usoz <manuelusozneri@gmail.com>
2022-02-07 17:00:41 -0300

    modified navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -17,7 +17,7 @@ class Navbar extends Component {
                 <div className="menu-icon" onClick={this.handleClick}>
                     <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
                 </div>
-                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                <ul className={this.state.clicked ? 'nav-menu' : 'nav-menu active'}>
                     {MenuItems.map((item, index) => {
                         return (
                             <li key={index}><a className={item.cName} href={item.url}>{item.title}</a></li>
