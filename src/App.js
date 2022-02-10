00e5a5422eb6f7eed8faf1b2f1448df62f11e9cb
Manuel Usoz <manuelusozneri@gmail.com>
2022-02-10 19:05:20 -0300

    modifico contador

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import './App.css';
 import Hero from './components/ListContainer';
-import ItemCount from './components/ItemCount';
+import Counter from './components/ItemCount';
 
 function App() {
   return (
       <div className="App">
       <Navbar/>
       <Hero title="Este es un titulo de ejemplo"/>
-      <ItemCount initial={1}/>
+      <Counter initial={1}/>
       </div>
       
     
--- src/components/ItemCount/index.jsx
+++ src/components/ItemCount/index.jsx
@@ -1,24 +1,44 @@
 import React from 'react';
 import './ItemCount.css';
 
-const ItemCount = (props) => {
-    const [setCount] = React.useState(props.initial);
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            initialValue: 1,
+        }
+    }
+    increment = () => {
+        this.setState({
+            initialValue: this.state.initialValue + 1
+        })
+    }
+    decrement = () => {
+        this.setState({
+            initialValue: this.state.initialValue - 1
+        })
+    }
+    render() {
         return(
             <div className="cart">
                 <h2>Producto</h2>
-                <h3>{props.initial}</h3>
+                <h3>{this.state.initialValue}</h3>
                 <div className="button-wrapper">
-                    <button onClick={() => setCount(props.initial - 1)} >-</button>
-                    <button onClick={() => setCount(props.initial + 1)} >+</button>
+                    <button onClick={this.decrement}>-</button>
+                    <button onClick={this.increment}>+</button>
                 </div>
             </div>
         )
+    }
+    
 }
+        
+
         
     
     
 
-export default ItemCount;
+export default Counter;
 
 
 
