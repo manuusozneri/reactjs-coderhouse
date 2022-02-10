f7d00beb2dd18775114eb7ff5ef4d30facd11651
Manuel Usoz <manuelusozneri@gmail.com>
2022-02-10 18:43:07 -0300

    modifico btn

--- src/components/ItemCount/index.jsx
+++ src/components/ItemCount/index.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import './ItemCount.css';
 
 const ItemCount = (props) => {
-    const [setCount] = React.useState(this.props.initial);
+    const [setCount] = React.useState(props.initial);
         return(
             <div className="cart">
                 <h2>Producto</h2>
                 <h3>{props.initial}</h3>
                 <div className="button-wrapper">
-                    <button onClick={() => setCount(this.props.initial - 1)}>-</button>
-                    <button onClick={() => setCount(this.props.initial + 1)}>+</button>
+                    <button onClick={() => setCount(props.initial - 1)} >-</button>
+                    <button onClick={() => setCount(props.initial + 1)} >+</button>
                 </div>
             </div>
         )
