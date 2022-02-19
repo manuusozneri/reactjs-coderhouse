45c3b5233aa1ef1d410c5924eac86a6c693a9e88
Manuel Usoz <manuelusozneri@gmail.com>
2022-02-18 23:46:03 -0300

    perfecciono entrega

--- src/components/ItemCount/index.jsx
+++ src/components/ItemCount/index.jsx
@@ -3,7 +3,37 @@ import './ItemCount.css';
 import MoreInfo from './MoreInfo.js';
 
 
-
+const array = [
+    {
+        id: 1,
+        title: 'Product X',
+        price: 40,
+        description: 'lorem ipsum',  
+        pictureUrl: 'http://example.com'
+    },
+    {
+        id: 2,
+        title: 'Product Y',
+        price: 90, 
+        description: 'lorem ipsum', 
+        pictureUrl: 'http://example.com'
+    },
+    {
+        id: 3,
+        title: 'Product Z',
+        price: 20, 
+        description: 'lorem ipsum', 
+        pictureUrl: 'http://example.com'
+    },
+    {
+        id: 4,
+        title: 'Product ZA',
+        price: 45, 
+        description: 'lorem ipsum', 
+        pictureUrl: 'http://example.com'
+    },
+    
+]
 const Counter = (props) => {
     var stock = props.stock 
     
@@ -29,22 +59,25 @@ const Counter = (props) => {
     
     
     
-    return(
+    return array.map((item) => (
+        
             <div className="cart">
-                <h2>Producto</h2>
-                <img src={props.imagen} alt={props.alt}></img>
-                <h3>{count}</h3>
-                <div className="button-wrapper">
-                    <button onClick={decValues}>-</button>
-                    <button onClick={onAdd}>Add to Cart</button>
-                    <button onClick={incValues}>+</button>
-                </div>
-                <div className="button-wrapper">
-                    <MoreInfo />
-                </div>
-                <h4>Stock: {props.stock}</h4>
+            <h2>{item.title}</h2>
+            <img src={props.imagen} alt={props.alt}></img>
+            <h3>{count}</h3>
+            <div className="button-wrapper">
+                <button onClick={decValues}>-</button>
+                <button onClick={onAdd}>Add to Cart</button>
+                <button onClick={incValues}>+</button>
             </div>
-    )
+            <div className="button-wrapper">
+                <MoreInfo />
+            </div>
+            <h4>Stock: {props.stock}</h4>
+            </div>
+        
+        
+));
 }
 
     
