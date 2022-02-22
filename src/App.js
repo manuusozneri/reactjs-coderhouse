10f472da6c0117bff1f226e04ed23aafaa418e81
Manuel Usoz <manuelusozneri@gmail.com>
2022-02-22 12:13:57 -0300

    Envio para correccion

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import HeroSection from './components/HeroSection/index.js';
 import Counter from './components/ItemCount';
@@ -12,7 +12,7 @@ function App() {
       <div className="App">
         <Router>
           <Navbar/>
-          <HeroSection/>
+          <Route exact path="/" element={HeroSection}/>
           <div className="productSection"> 
             <h1 id="productos">Mira Nuestros Productos</h1> 
             <div className="item-container">
